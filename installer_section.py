9e89a511f032cd4a63562808ca0cee80c39c5c24
Ikey Doherty <ikey@solusos.com>
2013-08-28 23:11:06 +0100

    Fix

--- installer_section.py
+++ installer_section.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  interface.py - Main Installer UI
+#  installer_section.py - Holds installer pages
 #  
 #  Copyright 2013 Ikey Doherty <ikey@solusos.com>
 #  
