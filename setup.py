5dae9e790322b3a9025ef4fb756b29f48cb1a7d7
Ikey Doherty <ikey@solus-project.com>
2015-12-02 03:56:48 +0000

    Release 5.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
             
 setup(name         = 'os-installer',
-      version      = '5.0',
+      version      = '5.1',
       author       = "Ikey Doherty",
       author_email = "ikey@solus-project.com",
       license      = "GPL-2.0",
