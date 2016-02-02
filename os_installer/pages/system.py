f10978bbc730e4a6e0b23f9172e1b53c83956865
Ikey Doherty <ikey@solus-project.com>
2016-02-02 00:47:09 +0000

    Shrink the abomination that is regex
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- os_installer/pages/system.py
+++ os_installer/pages/system.py
@@ -26,7 +26,8 @@ from .basepage import BasePage
 import re
 
 ValidHostnameRegex = \
-    "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
+    "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*" \
+    "([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
 
 
 class SystemPage(BasePage):
