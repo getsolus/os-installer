02e88d177a4641c93119466615abaec06a31889f
Ikey Doherty <ikey@solus-project.com>
2016-02-29 04:43:22 +0000

    mkfs fix
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- os_installer/installer.py
+++ os_installer/installer.py
@@ -56,10 +56,12 @@ class InstallerEngine:
                 if partition.format_as == "swap":
                     cmd = "mkswap %s" % partition.partition.path
                 else:
-                    if (partition.format_as == "jfs"):
+                    if partition.format_as == "jfs":
                         cmd = "mkfs.%s -q %s" % (partition.format_as, partition.partition.path)
-                    elif (partition.format_as == "xfs"):
+                    elif partition.format_as == "xfs":
                         cmd = "mkfs.%s -f %s" % (partition.format_as, partition.partition.path)
+                    elif partition.format_as in ["ext", "ext3", "ext4"]:
+                        cmd = "mkfs.%s -F %s" % (partition.format_as, partition.partition.path)
                     else:
                         cmd = "mkfs.%s %s" % (partition.format_as, partition.partition.path) # works with bfs, btrfs, ext2, ext3, ext4, minix, msdos, ntfs, vfat
 					
