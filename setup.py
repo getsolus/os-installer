51bda1351825ec2ef88a1f9a1d2bee7aabe32b91
Ikey Doherty <ikey@solus-project.com>
2016-02-29 05:18:40 +0000

    Release v5.3
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
             
 setup(name         = 'os-installer',
-      version      = '1.0',
+      version      = '5.3',
       author       = "Ikey Doherty",
-      author_email = "ikey@solusos.com",
-      license      = "GPLv2+",
+      author_email = "ikey@solus-project.com.com",
+      license      = "GPL-2.0",
       packages     = ['os_installer', 'os_installer.pages', 'os_installer.widgets'],
       scripts      = ['os-installer-gtk'],
 )
