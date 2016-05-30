946e77a678c9e3c18b4d6b188e3d8c7b914984c2
Ikey Doherty <ikey@solus-project.com>
2016-05-30 19:13:09 +0100

    Fix style
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- os_installer2/mainwindow.py
+++ os_installer2/mainwindow.py
@@ -10,8 +10,8 @@
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-
 from gi.repository import Gtk
+
 
 class MainWindow(Gtk.ApplicationWindow):
 
