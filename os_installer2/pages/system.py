3bec4016b480d62899dae1f436df4a5044dd579d
Ikey Doherty <ikey@solus-project.com>
2016-06-05 20:31:45 +0100

    Add the basic UI
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- os_installer2/pages/system.py
+++ os_installer2/pages/system.py
@@ -12,15 +12,50 @@
 #
 
 from .basepage import BasePage
+from gi.repository import Gtk
+import re
+
+# This is all we allow. OK.
+ValidHostnameRegex = "^[a-z_][a-z0-9_-]*[$]?$"
 
 
 class InstallerSystemPage(BasePage):
     """ System Settings page. """
 
     info = None
+    host_reg = None
+    host_entry = None
+    check_utc = None
 
     def __init__(self):
         BasePage.__init__(self)
+        self.host_reg = re.compile(ValidHostnameRegex)
+
+        wid_group = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
+
+        mbox = Gtk.VBox(0)
+        self.pack_start(mbox, True, True, 0)
+        mbox.set_halign(Gtk.Align.CENTER)
+        mbox.set_valign(Gtk.Align.CENTER)
+
+        # hostname section
+        host = Gtk.Frame()
+        host.set_shadow_type(Gtk.ShadowType.NONE)
+        host.set_label("What name should this computer use on the network?")
+        host.get_label_widget().set_margin_bottom(8)
+        mbox.pack_start(host, False, False, 10)
+
+        self.host_entry = Gtk.Entry()
+        self.host_entry.set_placeholder_text("Type the hostname here")
+        host.add(self.host_entry)
+
+        self.check_utc = Gtk.CheckButton.new_with_label(
+            "System clock uses UTC")
+        self.check_utc.set_margin_top(20)
+        mbox.pack_start(self.check_utc, False, False, 0)
+
+        wid_group.add_widget(host)
+        wid_group.add_widget(self.check_utc)
 
     def get_title(self):
         return "System Settings"
