f3d7ced8496e245e019242ec2d730b05dd27ef7b
Ikey Doherty <ikey@solus-project.com>
2016-06-05 13:12:20 +0100

    Begin splitting up the page implementation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- os_installer2/pages/partitioning.py
+++ os_installer2/pages/partitioning.py
@@ -12,19 +12,67 @@
 #
 
 from .basepage import BasePage
+from os_installer2.strategy import DualBootStrategy
 from os_installer2.strategy import EmptyDiskStrategy
 from os_installer2.strategy import WipeDiskStrategy
 from os_installer2.strategy import UseFreeSpaceStrategy
+from os_installer2.strategy import UserPartitionStrategy
+from gi.repository import Gtk
 import sys
+
+
+class DualBootPage(Gtk.VBox):
+    """ Used to manage the dual boot configuration settings,
+        essentially we're just here to resize the users partititon
+        and make room for Solus. """
+
+    def __init__(self):
+        Gtk.VBox.__init__(self)
+
+    def update_strategy(self, strat):
+        pass
+
+
+class ManualPage(Gtk.VBox):
+    """ Manual partitioning page, mostly TreeView with gparted proxy """
+
+    def __init__(self):
+        Gtk.VBox.__init__(self)
+
+    def update_strategy(self, strat):
+        pass
 
 
 class InstallerPartitioningPage(BasePage):
     """ Dual boot + partitioning page """
 
     info = None
+    stack = None
+
+    # Dual boot page
+    dbpage = None
+    mpage = None
 
     def __init__(self):
         BasePage.__init__(self)
+        self.stack = Gtk.Stack()
+        # Reduce lag
+        self.stack.set_transition_type(Gtk.StackTransitionType.NONE)
+        self.pack_start(self.stack, True, True, 0)
+
+        # Slow computers might show this page.. blank it
+        label = Gtk.Label.new("")
+        label.set_valign(Gtk.Align.CENTER)
+        label.set_halign(Gtk.Align.CENTER)
+        self.stack.add_named(label, "automatic")
+
+        self.dbpage = DualBootPage()
+        self.stack.add_named(self.dbpage, "dual-boot")
+
+        self.mpage = ManualPage()
+        self.stack.add_named(self.mpage, "manual")
+
+        self.stack.set_visible_child_name("manual")
 
     def get_title(self):
         return "Configure disks"
@@ -50,7 +98,16 @@ class InstallerPartitioningPage(BasePage):
         ]
         for sk in skips:
             if isinstance(info.strategy, sk):
+                self.stack.set_visible_child_name("automatic")
                 print("DEBUG: Skippable type")
                 self.info.owner.skip_page()
                 return
-        print("non-skippable strategy: {}".format(info.strategy.get_name()))
+        if isinstance(info.strategy, DualBootStrategy):
+            self.stack.set_visible_child_name("dual-boot")
+            self.dbpage.update_strategy(info.strategy)
+        elif isinstance(info.strategy, UserPartitionStrategy):
+            self.mpage.update_strategy(info.strategy)
+            self.stack.set_visible_child_name("manual")
+        else:
+            print("FATAL: Unknown strategy type!")
+            sys.exit(0)
