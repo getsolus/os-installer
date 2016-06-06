5f65fb93a20db6a3b84e588dc6a04237ee29c04e
Ikey Doherty <ikey@solus-project.com>
2016-06-06 19:57:15 +0100

    Fail test when using %s
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pep8 disk-test.py os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
-flake8 disk-test.py os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
+pep8 os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
+flake8 os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
+
+# check use of %s
+grep '%s' os-installer-gtk os_installer2/*.py os_installer2/pages/*.py && exit 1
