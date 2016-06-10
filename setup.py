d5f2784f70506d5ebf0957bdeca7f14eeebd0565
Ikey Doherty <ikey@solus-project.com>
2016-06-10 21:55:59 +0100

    Add setup.py so we can package this guy up
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name            = "os-installer",
+    version         = "5.4",
+    author          = "Ikey Doherty",
+    author_email    = "ikey@solus-project.com",
+    description     = ("Operating System Installer"),
+    license         = "GPL-3.0",
+    url             = "https://github.com/solus-project/os-installer",
+    packages        = ['os_installer2', 'os_installer2.pages'],
+    scripts         = ['os-installer-gtk'],
+    classifiers     = [ "License :: OSI Approved :: GPL-2.0 License"],
+    package_data    = {'os_installer2': ['data/*.png']},
+)
