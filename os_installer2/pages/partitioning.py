2d6e69c00712e5999799dedadacd6fea9aab49c1
Ikey Doherty <ikey@solus-project.com>
2016-06-12 11:24:08 +0100

    Revert "Don't chew the last 100 mb of disk.."
    This reverts commit 384c95fa9bfe9a6cb627d98dcaf800e0e69b50f9.

--- os_installer2/pages/partitioning.py
+++ os_installer2/pages/partitioning.py
@@ -236,10 +236,7 @@ class DualBootPage(Gtk.VBox):
 
         val = self.spin.get_value()
         avail = self.info.strategy.candidate_part.size
-
-        MB = 1000.0 * 1000.0
-        avail -= 100 * MB
-        GB = MB * 1000.0
+        GB = 1000.0 * 1000.0 * 1000.0
 
         nval = (avail / GB) - val
         ssize = format_size_local(nval * GB, double_precision=True)
