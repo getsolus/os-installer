57dd0ebb9092ccb380053d5262612a0fa21215ae
ZVNexus <zvnexus@outlook.com>
2020-01-25 12:47:27 -0500

    pep8 has been renamed to pycodestyle.
    "Use of the pep8 tool will be removed in a future release.
    Please install and use `pycodestyle` instead."

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pep8 os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
+pycodestyle os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
 flake8 os-installer-gtk  os_installer2/*.py os_installer2/pages/*.py || exit 1
 
 # check use of %s
